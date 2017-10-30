5e29caa0e3afc32f2c5f0e1740abdf7a3e637250
Robert Andersson <rbrt.andersson@outlook.com>
2017-10-30 20:13:45 +0100

    Add Google Benchmark external project

---
+++ externals/benchmark.py
@@ -0,0 +1,17 @@
+from build.model import CXXLibrary, GitClone
+
+source = GitClone(
+    "benchmark-source",
+    "https://github.com/google/benchmark.git")
+
+benchmark = CXXLibrary("benchmark")
+benchmark.add_dependency(source)
+benchmark.add_incpath("output/benchmark-source/include", publish=True)
+benchmark.add_incpath("output/benchmark-source/")
+benchmark.add_sources("output/benchmark-source/src", ".*\.cc$")
+benchmark.use_feature("language-c++11")
+benchmark.use_feature("optimize", level="full")
+benchmark.add_macro("NDEBUG")
+benchmark.add_macro("HAVE_STD_REGEX")
+benchmark.add_library("pthread", filter="linux", publish=True)
+
