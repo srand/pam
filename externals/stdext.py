e5c7af6e97dfe2472cc5854e182cab3e9a844a2c
Robert Andersson <rbrt.andersson@outlook.com>
2017-12-20 21:51:22 +0100

    add stdext-crypto external project

--- externals/stdext.py
+++ externals/stdext.py
@@ -3,6 +3,7 @@ from externals.googletest import googletest
 
 
 source_path = GitClone("stdext-path-source", "https://github.com/srand/stdext-path.git")
+source_crypto = GitClone("stdext-crypto-source", "https://github.com/srand/stdext-crypto.git")
 
 stdext_path = cxx_project(
     name = "stdext-path",
@@ -14,5 +15,21 @@ stdext_path_test = cxx_executable(
     name = "stdext-path-test",
     sources = ["output/stdext-path-source/test/test.cpp"],
     dependencies = [stdext_path, googletest],
+    features = ["language-c++11"]
+)
+
+stdext_crypto = cxx_library(
+    name = "stdext-crypto",
+    incpaths = [("output/stdext-crypto-source/include", {"publish": True})],
+    sources = ["output/stdext-crypto-source/src/crypto.cpp"],
+    features = ["language-c++11"],
+    libraries = [("crypto", {"publish": True, "filter": "linux"})],
+    dependencies = [source_crypto]
+)
+
+stdext_crypto_test = cxx_executable(
+    name = "stdext-crypto-test",
+    sources = ["output/stdext-crypto-source/test/test.cpp"],
+    dependencies = [googletest, stdext_crypto],
     features = ["language-c++11"]
 )
